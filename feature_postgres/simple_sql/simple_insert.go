f8177a1aae9a617ad1c20d8f63efc261b9d3b45b
Apr1lll <zarodov2307@mail.ru>
2026-03-01 21:26:11 +0300

    migrations init

---
+++ feature_postgres/simple_sql/simple_insert.go
@@ -0,0 +1,32 @@
+package simple_sql
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+// <- http
+// <- body
+// <- struct
+// <- args
+
+func InsertRow(
+	ctx context.Context,
+	conn *pgx.Conn,
+	task TaskModel,
+) error {
+	sqlQuery := `
+	INSERT INTO public.tasks (title, description, completed, created_at)
+	VALUES ($1, $2, $3, $4);
+	`
+	_, err := conn.Exec(
+		ctx,
+		sqlQuery,
+		task.Title,
+		task.Description,
+		task.Completed,
+		task.CreatedAt)
+
+	return err
+}
